3c7a4ed4c26dc5da9ca98c5075e701bcdae38d7a
CESCO <cescovalledev@gmail.com>
2022-01-25 21:37:43 -0600

    Mi primer script

--- Guia 1/secc.m
+++ Guia 1/secc.m
@@ -1,10 +1,10 @@
-function y=secc(x)
+function y = secc(x)
 %Este programa se utiliza para graficar la siguiente función
 %a trozos (o seccionada)
-if x<1
-    y=4-x^2;
-elseif 1<=x && x<3
-    y=3;
+if x < 1
+    y = 4 - (x^2);
+elseif 1 <= x && x < 3
+    y = 3;
 else
-    y=2*x-3;
+    y = 2*x - 3;
 end
